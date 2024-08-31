ccff9e11c95e52b2c3d3be18a123fdcad96b2fc3
husayn-codingal1 <mhusaynm@gmail.com>
2024-08-31 16:10:24 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,1 +1,8 @@
-print("husayn momin")
+def decimal_to_binary(n):
+  if n > 1:
+    decimal_to_binary(n // 2)
+  print(n % 2, end='')
+
+number = int(input("Enter a decimal number: "))
+
+decimal_to_binary(number)
